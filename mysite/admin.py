61b77d74d13c28c90d332bcdf9057c9d20a27f13
yasandr <zc.xtyzz@gmail.com>
2022-11-26 16:49:33 +0300

    after repair but nothing is working

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Product, User, Review, Order, Category, Brand
+from .models import Product, Review, Order, Category, Brand
 
 admin.site.register(Product)
-admin.site.register(User)
 admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(Category)
--- mysite/models.py
+++ mysite/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 
 class Product(models.Model):
     description = models.TextField()
     short_description = models.CharField(max_length=50)
-    price = models.FloatField()
-    category = models.IntegerField()
-    brand = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE
+    )
+    brand = models.ForeignKey(
+        'Brand',
+        on_delete=models.CASCADE
+    )
+    is_pesent = models.BooleanField(default=True)
 
 
 class Brand(models.Model):
@@ -15,22 +23,40 @@ class Brand(models.Model):
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
 
 class Review(models.Model):
-    product_id = models.IntegerField()
-    revie_text = models.TextField()
-
-
-class User(models.Model):
-    login_name = models.CharField(max_length=50)
-    id_review = models.IntegerField()
-    id_product_in_order = models.IntegerField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        default=""
+    )
+    product = models.ForeignKey(
+        'Product',
+        on_delete=models.CASCADE
+    )
+    rating = models.SmallIntegerField()
+    timestamp = models.DateTimeField()
+    text = models.TextField()
 
 
 class Order(models.Model):
-    product_id = models.IntegerField()
-    user_id = models.IntegerField()
-    is_paid = models.BooleanField()
-    order_date = models.DateTimeField()
+    product = models.ForeignKey(
+        'Product',
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    STATUS = [
+        ('not paid', 'order is not paid yet '),
+        ('paid', 'order is paid')
+    ]
+    status = models.CharField(
+        max_length=8,
+        choices=STATUS,
+        default = 'not paid'
+    )
+    timestamp = models.DateTimeField()
