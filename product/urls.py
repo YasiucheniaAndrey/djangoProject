f6eff35bfa9d0250f97f840ce496a14a96db2797
yasandr <zc.xtyzz@gmail.com>
2023-02-12 16:34:31 +0300

    add product templates

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'product'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:product_id>/', views.detail),
+    path('<int:product_id>/', views.detail, name='detail'),
+#cant change adress to spefifics/product_id
+#path specifics/<int:product_id>
+#with change in index.html
+# #<li><a href="{% url 'product:detail' product.id %}
 ]
--- product/views.py
+++ product/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Product
 
 
 def index(request):
-    return HttpResponse("Product page")
+    product_list = Product.objects.order_by('-id')
+    context = {
+        'product_list':product_list,
+    }
+    return render(request, 'product/index.html', context)
 
 def detail(request, product_id):
-    return HttpResponse(product_id)
+    product = get_object_or_404(Product,pk=product_id)
+    context = {'pd': product.description,
+               'psd': product.short_description,
+               'pp': product.price}
+    return render (request, 'product/detail.html', context)
+
+
+
