0660a1fcb64762c127551132380a6f3e1d0b2831
yasandr <zc.xtyzz@gmail.com>
2023-03-01 20:58:18 +0100

    customise user model

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -26,9 +26,8 @@ SECRET_KEY = 'django-insecure-y@f)ynp)iq!xhv(ymft#3gc3$t09lm_68j7dy@m3@4jdqa+vg-
 DEBUG = True
 
 ALLOWED_HOSTS = []
-EMAIL_HOST = "localhost"
-EMAIL_PORT = 1025
 
+   
 # Application definition
 
 INSTALLED_APPS = [
@@ -96,18 +95,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    # },
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "dashboard"
--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = 'product'
 urlpatterns = [
-#    path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/', views.ProdeuctDetailView.as_view(), name='detail'),
     path('', views.ProductListView.as_view()),
 ]
